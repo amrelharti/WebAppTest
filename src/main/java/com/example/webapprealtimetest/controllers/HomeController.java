46cbe47423621226179515345cedce12c8848c48
Amr Elharti <ELHARTI_AMR@emsi-edu.ma>
2024-07-15 13:49:54 +0100

    commit 5

--- src/main/java/com/example/webapprealtimetest/controllers/HomeController.java
+++ src/main/java/com/example/webapprealtimetest/controllers/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/index.html")
+    @GetMapping("/")
     public String index() {
         return "main";
     }
