79889382111965a243f8c0694da3186381fbae30
Amr Elharti <ELHARTI_AMR@emsi-edu.ma>
2024-07-16 18:18:08 +0100

    fixe end pts

--- src/main/java/com/example/webapprealtimetest/controllers/HomeController.java
+++ src/main/java/com/example/webapprealtimetest/controllers/HomeController.java
@@ -7,12 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/index.html")
+    @GetMapping("")
     public String index() {
         return "main";
     }
-    @GetMapping("/")
-    public String root() {
-        return "redirect:/index.html";
-    }
+
 }
